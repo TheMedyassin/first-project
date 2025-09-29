fa4e86932eb6dd2bb45eab2badd517b791bd3cf2
TheMedyassin <medyassin.alissaoui@gmail.com>
2025-09-29 15:44:23 +0100

    the buttons again

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 console.log("Hello from JavaScript!");
-document.getElementById("myBtn").addEventListener("click", () => {
-  alert("You clicked the button — nice!");
-});
 // Select the button using its ID
 const button = document.getElementById("AButton");
 
